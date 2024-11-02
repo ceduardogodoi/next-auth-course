471dd093304592a850617f7c82fba47af64a712d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-11-02 20:00:53 -0300

    feat: my account card

--- src/app/(logged-in)/my-account/page.tsx
+++ src/app/(logged-in)/my-account/page.tsx
@@ -1,3 +1,20 @@
-export default function MyAccountPage() {
-  return <div>My Account</div>;
+import { auth } from "@/auth";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+
+export default async function MyAccountPage() {
+  const session = await auth();
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>My Account</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <Label>Email Address</Label>
+        <div className="text-muted-foreground">{session?.user?.email}</div>
+      </CardContent>
+    </Card>
+  );
 }
