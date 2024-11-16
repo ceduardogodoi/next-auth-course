dbd087cc88994c271f837429fd3e15f51cd33662
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-11-16 16:31:51 -0300

    feat: save password reset token to the database

--- src/app/(logged-out)/password-reset/actions.ts
+++ src/app/(logged-out)/password-reset/actions.ts
@@ -5,6 +5,9 @@ import { eq } from "drizzle-orm";
 import { auth } from "@/auth";
 import { db } from "@/db/drizzle";
 import { users } from "@/db/users-schema";
+import { passwordResetTokens } from "@/db/password-reset-tokens-schema";
+
+const ONE_HOUR_IN_MS = 3_600_000;
 
 export async function passwordReset(emailAddress: string) {
   const session = await auth();
@@ -28,5 +31,21 @@ export async function passwordReset(emailAddress: string) {
   if (user == null) return;
 
   const passwordResetToken = randomBytes(32).toString("hex");
+  const tokenExpiry = new Date(Date.now() + ONE_HOUR_IN_MS);
+  await db
+    .insert(passwordResetTokens)
+    .values({
+      userId: user.id,
+      token: passwordResetToken,
+      tokenExpiry,
+    })
+    .onConflictDoUpdate({
+      target: passwordResetTokens.userId,
+      set: {
+        token: passwordResetToken,
+        tokenExpiry,
+      },
+    });
+
   console.log(passwordResetToken);
 }
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,2 @@
 export { users } from "./users-schema";
+export { passwordResetTokens } from "./password-reset-tokens-schema";
