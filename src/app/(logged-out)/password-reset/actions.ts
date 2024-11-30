e9aa39af4ba2f31de212fc5054cf8c1ee4e8800d
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-11-30 10:20:10 -0300

    feat: set up email sending with Resend

--- src/app/(logged-out)/password-reset/actions.ts
+++ src/app/(logged-out)/password-reset/actions.ts
@@ -6,6 +6,7 @@ import { auth } from "@/auth";
 import { db } from "@/db/drizzle";
 import { users } from "@/db/users-schema";
 import { passwordResetTokens } from "@/db/password-reset-tokens-schema";
+import { mailer } from "@/lib/email";
 
 const ONE_HOUR_IN_MS = 3_600_000;
 
@@ -46,6 +47,19 @@ export async function passwordReset(emailAddress: string) {
         tokenExpiry,
       },
     });
+
+  const resetLink = new URL("/update-password", process.env.SITE_BASE_URL);
+  resetLink.searchParams.set("token", passwordResetToken);
+
+  await mailer.sendMail({
+    from: "test@resend.dev",
+    subject: "Your password reset request",
+    to: emailAddress,
+    html: `Hey, ${emailAddress}! You requested to reset your password.
+Here's your password reset link. This link will expire in 1 hour:
+<a href="${resetLink.href}">${resetLink.href}</a>
+    `,
+  });
 
   console.log(passwordResetToken);
 }
