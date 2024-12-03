3c46fee70e9ebdcd7ff66519c9a7dd6506176436
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2024-12-03 20:14:15 -0300

    feat: render qr code to scan

--- src/app/(logged-in)/my-account/two-factor-auth-form/index.tsx
+++ src/app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { QRCodeSVG } from "qrcode.react";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { get2FaSecret } from "./actions";
 
 type TwoFactorAuthFormProps = {
   twoFactorActivated?: boolean;
@@ -12,9 +15,22 @@ export function TwoFactorAuthForm({
 }: TwoFactorAuthFormProps) {
   const [isActivated, setIsActivated] = useState(twoFactorActivated);
   const [step, setStep] = useState(1);
+  const [code, setCode] = useState("");
+  const { toast } = useToast();
 
   async function handleEnableClick() {
+    const response = await get2FaSecret();
+    if (response.error != null || response.error) {
+      toast({
+        variant: "destructive",
+        title: response.message,
+      });
+
+      return;
+    }
+
     setStep(2);
+    setCode(response.twoFactorSecret);
   }
 
   if (!isActivated) {
@@ -26,7 +42,27 @@ export function TwoFactorAuthForm({
           </Button>
         )}
 
-        {step === 2 && <div>Display QR Code</div>}
+        {step === 2 && (
+          <div>
+            <p className="text-xs text-muted-foreground my-2">
+              Scan the QR code below in the Google Authenticator app to activate
+              Two-Factor Authentication.
+            </p>
+
+            <QRCodeSVG value={code} />
+
+            <Button onClick={() => setStep(3)} className="w-full my-2">
+              I have scanned the QR code
+            </Button>
+            <Button
+              onClick={() => setStep(1)}
+              className="w-full my-2"
+              variant="outline"
+            >
+              Cancel
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
